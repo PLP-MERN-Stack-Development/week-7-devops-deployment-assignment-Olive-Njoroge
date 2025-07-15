0894244b4b421cb2307b4e72d3df99ec5a290758
Olive Njoroge <njorogeolive001@gmail.com>
2025-07-15 09:54:04 +0300

    Deleted

--- Live_Chat2.0/server/routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {getRoomMessages} = require('../controllers/messageController');
-
-router.get('/:roomId', getRoomMessages);
-
-module.exports = router;
--- Live_Chat2.0/server/routes/roomRoutes.js
+++
@@ -1,8 +0,0 @@
-const {createRoom, getRooms} = require ('../controllers/roomController');
-const express = require('express');
-const router = express.Router();
-
-router.post('/', createRoom);
-router.get('/', getRooms);
-
-module.exports = router;
